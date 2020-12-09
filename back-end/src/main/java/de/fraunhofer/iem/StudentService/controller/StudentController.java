4eb56218c7d51318e9f3dc44e4819dfb8241f792
harisnawaz94 <63430998+harisnawaz94@users.noreply.github.com>
2020-12-09 19:47:05 +0100

    Added @CrossOrigin
    to avoid cors error

--- back-end/src/main/java/de/fraunhofer/iem/StudentService/controller/StudentController.java
+++ back-end/src/main/java/de/fraunhofer/iem/StudentService/controller/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Collection;
 
@@ -21,11 +22,13 @@ public class StudentController {
     }
 
     @GetMapping("students")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Collection<Student>> getStudents() {
         Collection<Student> students = this.studentService.getStudents();
         return new ResponseEntity<>(students, HttpStatus.OK);
     }
     @PostMapping(path = "/update",consumes = "application/json",produces = "application/json")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Collection<Student>> setStudents(@RequestBody Student studentinput) {
         Collection<Student> students = this.studentService.setStudents(studentinput);
         return new ResponseEntity<>(students, HttpStatus.OK);
